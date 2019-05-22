a6cc851287ec6f19b36228ff8503314e83bd142a
Dmitry Brant <dbrant@wikimedia.org>
2019-05-22 08:52:51 -0400

    Add documentation.

--- qicstreamv2/Program.cs
+++ qicstreamv2/Program.cs
@@ -8,6 +8,98 @@ using System.Text;
 /// Decoder for QICStream (V2?) formatted tape images.
 /// 
 /// Copyright Dmitry Brant, 2019
+/// 
+/// 
+/// Brief outline of the format, to the best of my reverse-engineering ability:
+/// 
+/// * Every block of 0x8000 bytes ends with 0x402 bytes of extra data (for parity checking or
+///   some other kind of error correction?). In other words, for every 0x8000 bytes, only the
+///   first 0x7BFE bytes are useful data.
+/// * Little-endian.
+/// * At a high level, the archive basically consists of file and directory records in succession.
+///   Catalog information usually appears at the end of the archive, although catalog data may also
+///   be present in random places in the middle.
+/// 
+/// The structure of the archive consists of a volume header, followed by structures that are
+/// delimited by "control codes". For example, a control code might indicate that the bytes that
+/// follow are a file.
+/// It basically looks like this:
+/// 
+/// [Volume header]
+/// [Control code]
+/// [Data specific to the above control code]
+/// [Control code]
+/// [Data specific to the above control code]
+/// ....
+/// 
+/// Here are the possible control codes:
+/// 
+/// 1 - Start of contents area (no additional data after this code).
+/// 2 - Start of catalog area (no additional data after this code).
+/// 3 - Go up to the parent directory (i.e. the current directory stack should pop the
+///     last subdirectory) (no additional data).
+/// 5 - File. This code is followed by a file header, which should then be followed by one
+///     or more data chunks.
+/// 6 - Directory. This code is followed by a file header which will contain the name of this
+///     directory. When a new directory is reached, it becomes the "current" directory, and all
+///     subsequent files should be placed into it.  If there is already a "current" directory,
+///     then this directory should become a subdirectory. In other words, a "stack" of directories
+///     is built, with each directory entry pushed onto the top of the stack.
+/// 9 - Data chunk. This code is followed by a 16-bit "length" field which specifies the size of
+///     the data that follows it, and then the actual data that applies to the current file
+///     that is being read.  If a file is smaller than 0x10000 bytes, it should have just one
+///     data chunk. But if it's larger, then it will have multiple consecutive chunks.
+/// 
+/// Note: the space between two control codes may sometimes be padded with a 0 byte, possibly
+/// for alignment on a 16-bit boundary.
+/// 
+/// 
+/// == Volume Header structure ==
+/// 
+/// Byte offset (hex)     Meaning
+/// ----------------------------------------
+/// 4                     Magic characters "FSET".
+/// 
+/// 3C-3D                 Length of volume label.
+/// 3E-[variable]         Volume label.
+/// 
+/// ...after the volume label:
+/// 0-1                   Length of drive name.
+/// 2-[variable]          Drive name.
+/// 
+/// ...after the drive name:
+/// 0-5                   Padding bytes.
+/// --------------------------------------------------------------
+/// 
+/// 
+/// == File Header structure ==
+/// 
+/// Here is a breakdown of the file header:
+/// 
+/// Byte offset (hex)     Meaning
+/// ----------------------------------------
+/// 0                     Always seems to be 1, but does not appear to be important.
+/// 
+/// 1-2                   Size of this header, NOT INCLUDING these first three bytes.
+/// 
+/// 3-4                   File attributes (the usual DOS attribute bits).
+/// 
+/// 7-A                   File date, encoded as time_t (seconds since 1970 epoch).
+/// 
+/// B-12                  64-bit file size. Should be 0 if this is a directory entry.
+/// 
+/// 19-[variable]         File or directory name, the length of which is determined by the "size"
+///                       field in this header.
+/// --------------------------------------------------------------
+/// 
+/// 
+/// == Catalog data ==
+/// 
+/// If a catalog area is reached (control code 2), then it will be followed by a series of
+/// file headers, but no contents. All of this can be safely skipped until a new contents area
+/// (control code 1) is reached.
+/// 
+/// 
 /// </summary>
 namespace QicStreamV2
 {
