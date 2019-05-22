34c2203b6532bb57d9b71e01f78e60e8e52fb843
Dmitry Brant <dbrant@wikimedia.org>
2019-05-21 23:19:59 -0400

    Add documentation.

--- qicstreamv4/Program.cs
+++ qicstreamv4/Program.cs
@@ -8,6 +8,98 @@ using System.Text;
 /// Decoder for QICStream (V4?) format.
 /// 
 /// Copyright Dmitry Brant, 2019
+/// 
+/// 
+/// Brief outline of the format, to the best of my reverse-engineering ability:
+/// 
+/// * Every file and directory record is aligned to a block boundary (512 bytes).
+/// * Little-endian.
+/// * Here is a high-level organization of the stream:
+/// 
+/// [Volume header (1 block, 512 bytes)]
+/// [File or directory entry (aligned to block)]
+/// [File or directory entry (aligned to block)]
+/// [File or directory entry (aligned to block)]
+/// ....
+/// 
+/// Each file or directory entry starts with a header. If it's a file (not a directory) then
+/// the file contents will follow immediately after the header.
+/// 
+/// == File Header structure ==
+/// 
+/// Byte offset (hex)     Meaning
+/// ----------------------------------------
+/// 0                     A value of 9 means it's a file header.
+/// 
+/// 18                    A value of 0 seems to mean that the file contents will appear immediately
+///                       following this header. However, a value of 1 seems to mean that the header
+///                       will be followed by 0x24 extra bytes of data, and then followed by the contents.
+/// 
+/// 4E-51                 32-bit size of the file.
+/// 
+/// 52-55                 Another 32-bit size of the file. If this size is nonzero, then this is the size
+///                       that should be trusted, and not the other size fields. If this size is zero, then
+///                       the other size fields should be used.
+/// 
+/// 56-5C                 64-bit size of the file.
+/// 
+/// 5F                    File attributes (the usual DOS attribute bits).
+/// 
+/// 68-69                 Creation year.
+/// 6A-6B                 Creation month.
+/// 6C-6D                 Creation day.
+/// 6E-6F                 Creation hour.
+/// 70-71                 Creation minute.
+/// 72-73                 Creation second.
+/// 
+/// 78-79                 Modification year.
+/// 7A-7B                 Modification month.
+/// 7C-7D                 Modification day.
+/// 7E-7F                 Modification hour.
+/// 80-81                 Modification minute.
+/// 82-83                 Modification second.
+/// 
+/// 9A-9B                 File name length, including null terminating character.
+/// 
+/// 9C-9D                 16-bit size of this header, which should be 0x9E.
+/// ------------------------------------------------------------------------------
+/// 
+/// The header is immediately followed by the file name (the length of which is given in the header).
+/// 
+/// If the file header (plus file name) doesn't end on a two-byte boundary, then an extra
+/// padding byte will be present.
+/// 
+/// The header (plus file name) is followed by the file contents, unless the header specifies
+/// that extra data is present (see offset 18 in the header), in which case there will be an
+/// extra 0x24 bytes, and then the file contents will begin.
+/// 
+/// == Directory Header structure ==
+/// 
+/// Byte offset (hex)     Meaning
+/// ----------------------------------------
+/// 0                     A value of 8 means it's a directory header.
+/// 
+/// 96-97                 Directory name length, including null terminating character. This could
+///                       be 1, which would be just the null character, meaning that this is the
+///                       root directory of the archive.
+///                       This name may also contain one or more null characters in the middle, to
+///                       denote subdirectories. For example, the name "FOO\0BAR\0BAZ" would translate
+///                       into the directory structure of FOO\BAR\BAZ.
+/// 
+/// 98-99                 16-bit size of this header, which should be 0x9A.
+/// ------------------------------------------------------------------------------
+/// 
+/// This header is followed immediately by the name of the directory (the length of which is given
+/// in the header).
+/// 
+/// There are no other contents that follow the directory header (the next file or directory header will
+/// start on the next block boundary).
+/// 
+/// When a directory header is present, that directory becomes the "current" directory, and all subsequent
+/// files shall be placed in the current directory.
+/// 
+/// 
+/// 
 /// </summary>
 namespace QicStreamV4
 {
