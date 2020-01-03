a4845b3111ef70a9233cfa8597f3ce18b7d764a5
Dmitry Brant <dbrant@wikimedia.org>
2020-01-03 17:25:25 -0500

    Update copyright year.

--- qicstreamv4/Properties/AssemblyInfo.cs
+++ qicstreamv4/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("QicStreamV4")]
-[assembly: AssemblyCopyright("Copyright © Dmitry Brant")]
+[assembly: AssemblyCopyright("Copyright © 2019-2020 Dmitry Brant")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
