bda182dbaa2fc05bcd81a4fe33d22afadc2da8ab
Dmitry Brant <me@dmitrybrant.com>
2022-04-20 08:49:51 -0400

    Add a few more utility functions.

--- QicUtils/Utils.cs
+++ QicUtils/Utils.cs
@@ -51,7 +51,15 @@ namespace QicUtils
 			return new DateTime(1970, 1, 1).AddSeconds(timeT);
 		}
 
-		public static DateTime GetMtfDateTime(byte[] bytes, int offset)
+        public static DateTime DateTimeFrom1904(long time)
+        {
+            var epoch = new DateTime(1904, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            try { epoch = epoch.AddSeconds(time); }
+            catch { }
+            return epoch;
+        }
+
+        public static DateTime GetMtfDateTime(byte[] bytes, int offset)
 		{
 			int year = (bytes[offset] << 6) | (bytes[offset + 1] >> 2);
 			int month = ((bytes[offset + 1] & 0x3) << 2) | (bytes[offset + 2] >> 6);
@@ -64,5 +72,65 @@ namespace QicUtils
 			catch { date = DateTime.Now; }
 			return date;
 		}
-	}
+
+        private static UInt16 conv_endian(UInt16 val)
+        {
+            UInt16 temp;
+            temp = (UInt16)(val << 8); temp &= 0xFF00; temp |= (UInt16)((val >> 8) & 0xFF);
+            return temp;
+        }
+        private static UInt32 conv_endian(UInt32 val)
+        {
+            UInt32 temp = (val & 0x000000FF) << 24;
+            temp |= (val & 0x0000FF00) << 8;
+            temp |= (val & 0x00FF0000) >> 8;
+            temp |= (val & 0xFF000000) >> 24;
+            return (temp);
+        }
+        private static UInt64 conv_endian(UInt64 val)
+        {
+            UInt64 temp = (val & 0xFF) << 56;
+            temp |= (val & 0xFF00) << 40;
+            temp |= (val & 0xFF0000) << 24;
+            temp |= (val & 0xFF000000) << 8;
+            temp |= (val & 0xFF00000000) >> 8;
+            temp |= (val & 0xFF0000000000) >> 24;
+            temp |= (val & 0xFF000000000000) >> 40;
+            temp |= (val & 0xFF00000000000000) >> 56;
+            return (temp);
+        }
+
+
+        public static UInt16 LittleEndian(UInt16 val)
+        {
+            if (BitConverter.IsLittleEndian) return val;
+            return conv_endian(val);
+        }
+        public static UInt32 LittleEndian(UInt32 val)
+        {
+            if (BitConverter.IsLittleEndian) return val;
+            return conv_endian(val);
+        }
+        public static UInt64 LittleEndian(UInt64 val)
+        {
+            if (BitConverter.IsLittleEndian) return val;
+            return conv_endian(val);
+        }
+
+        public static UInt16 BigEndian(UInt16 val)
+        {
+            if (!BitConverter.IsLittleEndian) return val;
+            return conv_endian(val);
+        }
+        public static UInt32 BigEndian(UInt32 val)
+        {
+            if (!BitConverter.IsLittleEndian) return val;
+            return conv_endian(val);
+        }
+        public static UInt64 BigEndian(UInt64 val)
+        {
+            if (!BitConverter.IsLittleEndian) return val;
+            return conv_endian(val);
+        }
+    }
 }
