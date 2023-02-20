8da7b874773b12ed9f378f1e9837b9d502e8737e
Dmitry Brant <me@dmitrybrant.com>
2023-02-20 15:27:32 -0500

    Tighten up.

--- txver45/TxDecompressor.cs
+++ txver45/TxDecompressor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace txver45
 {
@@ -54,29 +55,22 @@ namespace txver45
             this.stream = stream;
         }
 
-        void pushValue(int i)
+        public void DecompressTo(Stream outStream)
         {
-            pushValue(new List<byte> { (byte)i });
-        }
-
-        void pushValue(List<byte> list)
-        {
-            history[histPtr] = list;
-            histPtr++;
-            if (histPtr >= history.Length) { histPtr = 0; }
-        }
-
-        bool execute(int instr, int offset, Stream inStream, Stream outStream)
-        {
-            if (instr == 0)
+            while (stream.Position < stream.Length)
             {
-                // literal byte
-                outStream.WriteByte((byte)offset);
-                pushValue(offset);
-            }
-            else if (instr >= 1 && instr < 0xF0)
-            {
-                offset += ((instr - 1) * 0x100);
+                int offset = NextNumBits(8);
+                int instr = NextNumBits(4);
+
+                if (instr == 0)
+                {
+                    // literal byte
+                    outStream.WriteByte((byte)offset);
+                    pushValue(offset);
+                    continue;
+                }
+
+                offset += (instr - 1) * 0x100;
 
                 if (offset == 0)
                 {
@@ -88,20 +82,28 @@ namespace txver45
                 else if (offset == 1)
                 {
                     // end of compressed data
-                    return false;
+                    break;
                 }
                 else if (offset == 2)
                 {
-                    // read next byte, and take that many next bytes literally
-                    int n = inStream.ReadByte();
-
-                    if (n == 0)
+                    // repeat the next number of literal bytes
+                    int n;
+                    if (Aligned())
+                        n = NextNumBits(8);
+                    else
                     {
-                        throw new ApplicationException("Warning: empty run length.");
+                        n = NextNumBits(4);
+                        // if offset is 0, then the actual number is the next byte
+                        if (n == 0)
+                            n = NextNumBits(8);
                     }
+                    // if offset is 0, then the actual number is 0x100
+                    if (n == 0)
+                        n = 0x100;
+
                     for (int i = 0; i < n; i++)
                     {
-                        byte a = (byte)inStream.ReadByte();
+                        byte a = (byte)stream.ReadByte();
                         pushValue(a);
                         outStream.WriteByte(a);
                     }
@@ -113,7 +115,7 @@ namespace txver45
 
                     if (offset < 0 || history[offset] == null)
                     {
-                        throw new ApplicationException("Warning: invalid offset?");
+                        throw new ApplicationException("Invalid offset");
                     }
                     else
                     {
@@ -124,92 +126,32 @@ namespace txver45
 
                         offset++;
                         if (offset < history.Length && (history[offset] != null))
-                        {
                             newList.Add(history[offset].First());
-                        }
                         else
-                        {
                             newList.Add(list.First());
-                        }
 
                         for (int i = 0; i < newList.Count; i++)
-                        {
                             outStream.WriteByte(newList[i]);
-                        }
+
                         pushValue(newList);
                     }
                 }
             }
-            else
-            {
-                throw new ApplicationException("Warning: invalid instruction?");
-            }
-            return true;
         }
 
-
-        public void DecompressTo(Stream outStream)
+        private void pushValue(int i)
         {
-            int a, instr, offset;
-
-            while (stream.Position < stream.Length)
-            {
-                offset = NextNumBits(8);
-                instr = NextNumBits(4);
-
-                if (offset == 2 && instr < 2)
-                {
-                    if (instr == 1)
-                    {
-                        // repeat the next number of literal bytes
-
-                        if (Aligned())
-                        {
-                            offset = NextNumBits(8);
-                        }
-                        else
-                        {
-                            offset = NextNumBits(4);
-                            // if offset is 0, then the actual number is the next byte
-                            if (offset == 0)
-                            {
-                                offset = NextNumBits(8);
-                            }
-                        }
-                        // if offset is 0, then the actual number is 0x100
-                        if (offset == 0)
-                        {
-                            offset = 0x100;
-                        }
-
-                        if (offset == 1)
-                        {
-                            throw new ApplicationException("Warning: offset seems wrong?");
-                        }
-
-                        for (int i = 0; i < offset; i++)
-                        {
-                            a = stream.ReadByte();
-                            outStream.WriteByte((byte)a);
-                            pushValue(a);
-                        }
-                    }
-                    else
-                    {
-                        // literal 02 byte
-                        outStream.WriteByte((byte)offset);
-                        pushValue((byte)offset);
-                    }
-                }
-                else
-                {
-                    if (!execute(instr, offset, stream, outStream))
-                        return;
-                }
-            }
+            pushValue(new List<byte> { (byte)i });
         }
 
-        public bool Aligned()
+        private void pushValue(List<byte> list)
+        {
+            history[histPtr] = list;
+            histPtr++;
+            if (histPtr >= history.Length) { histPtr = 0; }
+        }
+
+        private bool Aligned()
         {
             return curBitMask > 0x80;
         }
@@ -226,7 +168,7 @@ namespace txver45
             return ret;
         }
 
-        public int NextNumBits(int bits)
+        private int NextNumBits(int bits)
         {
             int num = 0;
             for (int i = 0; i < bits; i++)
